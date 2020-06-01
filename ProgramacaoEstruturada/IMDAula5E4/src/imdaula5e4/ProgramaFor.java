5973acac2c727f0c5f31fa1b95f907894e6744c2
n-jessica <n.jessica0411@gmail.com>
2020-06-01 19:05:51 -0300

    Aula 05 - Comandos de iteração - Exercicio 4

---
+++ ProgramacaoEstruturada/IMDAula5E4/src/imdaula5e4/ProgramaFor.java
@@ -0,0 +1,23 @@
+
+package imdaula5e4;
+
+/**
+ *
+ * @author jessica
+ */
+public class ProgramaFor {
+
+    /**
+     * @param args the command line arguments
+     * 
+     * Algoritmo conta de 1 a 10
+     */
+    public static void main(String[] args) {
+        int i = 1;
+		while (i <= 10) {
+			System.out.println(i);
+			i = i + 1;
+		}
+    }
+    
+}
