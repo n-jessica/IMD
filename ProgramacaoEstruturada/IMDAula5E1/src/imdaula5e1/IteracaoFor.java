5a8381e889d1fe8df10a9065ea1f0f09956dbe0d
n-jessica <n.jessica0411@gmail.com>
2020-06-01 13:30:06 -0300

    Aula 05 - Comandos de iteração - Exercicio 1

---
+++ ProgramacaoEstruturada/IMDAula5E1/src/imdaula5e1/IteracaoFor.java
@@ -0,0 +1,21 @@
+
+package imdaula5e1;
+
+/**
+ *
+ * @author jessica
+ */
+public class IteracaoFor {
+
+    /**
+     * @param args the command line arguments
+     * 
+     * Algoritmo mostra o funcionamento do comando de iteração FOR.
+     */
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            System.out.println(+(i+1)+" Iniciando estudo dos comandos de iteração!!");
+        }
+    }
+    
+}
