03c705bc1285e9b9129587fb1fd6343bc06a8d47
n-jessica <n.jessica0411@gmail.com>
2020-06-13 14:24:23 -0300

    Aula 08 - Funções e procedimentos - Exercício 2

---
+++ ProgramacaoEstruturada/IMDAula8E2/src/imdaula8e2/ProgramaFuncaoQuadrado.java
@@ -0,0 +1,37 @@
+package imdaula8e2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jessica
+ */
+public class ProgramaFuncaoQuadrado {
+
+    /**
+     * @param args the command line arguments
+     * 
+     * Esse programa declara uma função chamada quadrado, que, dado um número, 
+     * retorna o valor desse número multiplicado por ele mesmo, ou seja, esse número elevado ao quadrado.
+     */
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite um número:");
+        double num = leitor.nextDouble();
+        System.out.println("O quadrado desse número é:" + quadrado(num));
+        // ... Aqui o programa faz um monte de coisa 
+        // ... Aqui o programa faz um monte de coisa 
+        // ... Aqui o programa faz um monte de coisa 
+        // ... Aqui o programa faz um monte de coisa 
+        // ... Depois de muito código, precisa-se calcular 
+        // ...    novamente o quadrado 
+        double num2 = leitor.nextDouble();
+        System.out.println("O quadrado do segundo número digitado é:" + quadrado(num2));
+    }
+
+    public static double quadrado(double numero) {
+        return numero * numero;
+    }
+}
+
+
