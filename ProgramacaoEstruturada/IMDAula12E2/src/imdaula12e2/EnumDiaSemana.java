d7875b7a8713231a2c4ff891945d0d8c660630a9
n-jessica <n.jessica0411@gmail.com>
2020-06-14 14:59:18 -0300

    Aula 12 - Registros e enumerações - Exercício 2

---
+++ ProgramacaoEstruturada/IMDAula12E2/src/imdaula12e2/EnumDiaSemana.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imdaula12e2;
+
+/**
+ *
+ * @author jessica
+ */
+public enum EnumDiaSemana {
+    DOMINGO, SEGUNDA_FEIRA, TERCA_FEIRA,
+    QUARTA_FEIRA, QUINTA_FEIRA, SEXTA_FEIRA, SABADO
+}
