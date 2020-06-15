9638728dedea076deacd321cbe27882083ddbabf
n-jessica <n.jessica0411@gmail.com>
2020-06-15 16:36:56 -0300

    POO - primeiros Atributos

---
+++ ProgramaçãoOrientadaaObjeto/Fabrica/src/Fabrica/Carro.java
@@ -0,0 +1,16 @@
+
+package Fabrica;
+
+/**
+ *
+ * @author jessica
+ * 
+ * Iniciando estudos de Programação Orientada a Objetos
+ */
+public class Carro {
+    //Atributos
+    String tipo;
+    String cor;
+    String placa;
+    int numPortas;
+}
