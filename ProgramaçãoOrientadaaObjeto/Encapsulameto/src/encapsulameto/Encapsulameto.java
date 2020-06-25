ff7fb5caa655244b0a9051f2e3a3731b7c8cbc4c
n-jessica <n.jessica0411@gmail.com>
2020-06-25 20:51:25 -0300

    Aula 05 - Encapsulamento - Exercício

---
+++ ProgramaçãoOrientadaaObjeto/Encapsulameto/src/encapsulameto/Encapsulameto.java
@@ -0,0 +1,21 @@
+package encapsulameto;
+
+/**
+ *
+ * @author jessica
+ */
+public class Encapsulameto {
+
+    /**
+     * @param args the command line arguments Algorítmo mais complento
+     * demonstrando o uso do encapsulamento Criado por acompanhamento de video
+     * aula.
+     */
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+        c.fecharMenu();
+
+    }
+}
