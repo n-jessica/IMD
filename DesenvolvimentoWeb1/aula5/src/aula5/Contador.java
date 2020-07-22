e8ed6863208f6ef8d714686b07957b83624c0178
n-jessica <n.jessica0411@gmail.com>
2020-07-22 14:31:17 -0300

    Aula 05 - Introdução e Recursos JavaServer Pages - Exercícios 1 e 2 - Números aleatórios e Contador

---
+++ DesenvolvimentoWeb1/aula5/src/aula5/Contador.java
@@ -0,0 +1,12 @@
+package aula5;
+
+public class Contador {
+	private static int contagem;
+
+	   public static void novoAcesso() {
+	     contagem = contagem + 1;
+	   }
+	   public static int getQuantidadeAcessos() {
+	     return contagem;
+	   }
+}
